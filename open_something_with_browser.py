6f08f480f689279cadf632fa181595a97f6df71e
Raaj Asrieeth MR <asrieethraaj@gmail.com>
2020-08-11 11:51:49 +0530

    Add files via upload
    updated

--- open_something_with_browser.py
+++ open_something_with_browser.py
@@ -1,9 +1,18 @@
 import webbrowser
 
 def openSomething(link):
-	url = f"www.{str(link.lower())}.com"
-	x = webbrowser.open(url)
-	return x
+	try:
+		url = f"www.{str(link.lower())}.com"#formats to a typical  address
+		if "http" in link:#if complete link is provided
+			x = webbrowser.open(link)
+		elif "." in link:#if a non http link is given
+			x = webbrowser.open(link)
+
+		elif "https" not in link:#only name is specified
+			x = webbrowser.open(url)
+		return x
+	except Exception :
+		return "Error ocurred"
 
 
-print(openSomething("youtube"))
+print(openSomething("google"))
