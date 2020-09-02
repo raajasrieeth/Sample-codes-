f239b3bce7997d383a89824647b9fed1669944ad
Raaj Asrieeth MR <asrieethraaj@gmail.com>
2020-09-02 07:33:59 +0530

    Create recurs_factorial.py

---
+++ recurs_factorial.py
@@ -0,0 +1,9 @@
+#recursive approach to find factorial.
+def factorial(num = int(input("Enter a number for the factorial."):
+    if num == 1:
+        return 0
+    else:
+        return(num*factorial(num-1)
+        
+
+factorial()
